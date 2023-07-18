351cae4e42244852cc5468e69601c091db2a662e
Richie911 <oohrichmond.k@gmail.com>
2023-07-18 16:39:50 -0700

    print_last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,14 +12,14 @@ int print_last_digit(int n)
 {
 	int last_digit = n % 10;
 
-	if (l < 0)
+	if (last_digit < 0)
 	{
-		_putchar(-l + 48);
-		return (-l);
+		_putchar(-last_digit + 48);
+		return (-last_digit);
 	}
 	else
 	{
-		_putchar(l + 48);
-		return (l);
+		_putchar(last_digit + 48);
+		return (last_digit);
 	}
 }
