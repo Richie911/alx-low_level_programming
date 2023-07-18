694674aa995990514e1ae83f2a5d66914158fcca
Richie911 <oohrichmond.k@gmail.com>
2023-07-18 15:42:41 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets 10X.
+ *
+ * Return: 0
+ */
+
+void print_alphabet_x10(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 97; j < 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
