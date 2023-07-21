5f9a26fda205153d5e054ce279bf3e7cc773d8a6
Richie911 <oohrichmond.k@gmail.com>
2023-07-20 17:01:08 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -22,12 +22,7 @@ void print_line(int n)
 		{
 			for (j = 0; j < n; j++)
 			{
-				if (j == i)
-				{
-					_putchar('_');
-				}
-				else if (j < i)
-					_putchar(' ');
+				_putchar('_');
 			}
 			_putchar('\n');
 		}
