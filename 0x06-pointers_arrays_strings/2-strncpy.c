819414a023c7bf47a679d3804f900bf92f229428
Richie911 <oohrichmond.k@gmail.com>
2023-07-27 20:05:12 -0700

    string copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+/**
+ * _strncpy - copies a string.
+ * @dest: string
+ * @src: string
+ * @n: bytes to be copied
+ * Return: pointer
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (stc[i++])
+	{
+		j++;
+	}
+
+	for (i = 0; src[i] && i < n; i++)
+	{
+		dest[i] = src[i]
+	}
+	for (i = j; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
