5ac1c25acda5fa2d4a389f8f1fbd115cc91a7306
Richie911 <oohrichmond.k@gmail.com>
2023-07-27 20:18:18 -0700

    compare strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+/**
+ * _strcmp - compares two strings
+ * @s1: string
+ * @s2: string
+ * Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
