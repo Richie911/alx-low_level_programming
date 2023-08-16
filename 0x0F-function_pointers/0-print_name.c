3fa7e3f39429910ecf6448c3af3a1ec0017384b3
Richie911 <oohrichmond.k@gmail.com>
2023-08-16 15:27:49 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_name - print name
+ * @name: name
+ * @f: func
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	void (*newFunc)(char*) = f;
+	newFunc(name);
+}
