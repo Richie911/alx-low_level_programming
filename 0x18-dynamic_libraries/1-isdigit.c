d1412d793ccad49866a9cc2f86f3e6f2be1064db
Richie911 <56179749+Richie911@users.noreply.github.com>
2023-10-16 22:26:21 +0000

    library

---
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: The character to be checked
+ * Return: 1 for a digit character or 0 for anything else
+ */
+
+int _isdigit(int c)
+
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+		return (0);
+}
